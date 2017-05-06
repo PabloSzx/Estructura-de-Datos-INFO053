311f978a8d9a3f67c38e94a5f0af750108ad7f07
Pablo Sáez Parra <pablosaez1995@gmail.com>
2017-05-06 18:07:17 -0300

    solucion clase 6

---
+++ Clase 6/arbol.h
@@ -0,0 +1,111 @@
+#ifndef arbol_H
+#define arbol_H
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include "pila.h"
+
+using namespace std;
+
+typedef struct arbol{
+    nodo *c;
+    int largo;
+}arbol;
+
+nodo* nodo_crear(int num){
+    nodo *a = (nodo*)malloc(sizeof(nodo));
+    a->num = num;
+    a->right = NULL;
+    a->left = NULL;
+    return a;
+}
+
+void nodo_insertar(nodo* q, nodo* n) {
+  if (n->num > (q->num)) {
+    if (q->right != NULL) {
+      nodo_insertar(q->right, n);
+    } else {
+      q->right = n;
+    }
+  } else if (n->num < (q->num)) {
+    if (q->left != NULL) {
+      nodo_insertar(q->left, n);
+    } else {
+      q->left = n;
+    }
+  }
+}
+
+arbol* arbol_crear(int n){
+    arbol* q = (arbol*)malloc(sizeof(arbol));
+    nodo* nd = nodo_crear(n);
+    q->c = nd;
+    q->largo = 0;
+    return q;
+}
+
+
+void arbol_insertar(arbol** q, int num){
+    nodo *n = nodo_crear(num);
+    nodo *raiz = (*q)->c;
+
+    nodo_insertar(raiz, n);
+
+    (*q)->largo++;
+}
+
+nodo* buscar_nodo(arbol** q, int num) {
+  pila *pil = pila_crear();
+
+  //Algoritmo en preorden
+  nodo *p = (*q)->c;
+  insertar_pila(&pil, p);
+  while (!pila_vacia(&pil)) {
+    p = eliminar_pila(&pil);
+    //Visitar(p) start
+    if (p->num == num) {
+      return p;
+    }
+    //Visitar(p) end
+    if (p->right != NULL) {
+      insertar_pila(&pil, p->right);
+    }
+    if (p->left != NULL) {
+      insertar_pila(&pil, p->left);
+    }
+  }
+
+  cout << "\nNodo no encontrado!!";
+  return NULL;
+}
+
+void imprimir_arbol_preorden(arbol** q) {
+  pila *pil = pila_crear();
+
+  //Algoritmo en preorden
+  nodo *p = (*q)->c;
+  insertar_pila(&pil, p);
+  while (!pila_vacia(&pil)) {
+    p = eliminar_pila(&pil);
+    //Visitar(p) start
+    cout << "(" << p->num << ")";
+    //Visitar(p) end
+    if (p->right != NULL) {
+      insertar_pila(&pil, p->right);
+    }
+    if (p->left != NULL) {
+      insertar_pila(&pil, p->left);
+    }
+
+    if (!pila_vacia(&pil)) {
+      cout << " - ";
+    }
+  }
+
+  cout <<"\n";
+
+}
+
+#endif
---
+++ Clase 6/main_usuario.cc
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "arbol.h"
+#include "pila.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  int start;
+  cout << "\nIngrese un valor inicial para el arbol: ";
+  cin >> start;
+  arbol *arb = arbol_crear(start);
+
+  while (true) {
+    cout << "\nLista de comandos: ";
+    cout << "\n0.- Ingresar un elemento.";
+    cout << "\n1.- Imprimir el arbol en preorden.";
+
+    int choice, n;
+    cout << "\n\nIngrese un numero segun el comando que desea realizar: ";
+    cin >> choice;
+    cout << "\n";
+    switch (choice) {
+      case 0:
+        cout <<"\nIngrese el numero que desea ingresar en el arbol: ";
+        cin >> n;
+        arbol_insertar(&arb, n);
+        break;
+      case 1:
+        imprimir_arbol_preorden(&arb);
+        cout << "\nEl arbol tiene " << arb->largo << " nodos\n";
+        break;
+      default:
+        cout << "\nNo se identifico el comando a realizar!!";
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
